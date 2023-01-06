ab466b090af4a8f3c067bf1a4191957e7cf40c3c
Connie Wong <conniehhw@gmail.com>
2023-01-06 15:30:46 -0500

    add functions check time & styling

--- script.js
+++ script.js
@@ -1,37 +1,111 @@
-//const d = new Date();
-//document.getElementById("currentDay").innerHTML = d.getFullYear;
-
-const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
 const d = new Date();
-let month = months[d.getMonth()];
-document.getElementById("currentDay").innerHTML = month;
+document.getElementById("currentDay").innerHTML = d;
 
-// $(document).ready(function(){ ---> use for when time is past/present/future
-//   $("button").click(function(){
-//     $("h1, h2, p").addClass("blue");
-//     $("div").addClass("important");
-//   });
-// });
+let hour = new Date().getHours();
+document.getElementById("demo").innerHTML = hour;
 
-// let checkTime = function () {
-//     if(Date.now === Date.now)
-//     $("col-9").addClass("green")};
 
-//     if(Date.now <)
-//     $("col-9").addClass("red");
-//     else( $("col-9").addClass("gray")
+// for (let i = 9; i < 17; i++) {
+//     // text += "The number is " + i + "<br>";
+
+//   }
+
+// let time = document.getElementById("time").textContent;
+//     document.getElementById("demo4").innerHTML = time;
+
+// for (let i = time; i > 17; i++) {
+
+function myFunction4 (){
+    let time = document.getElementById("time").textContent;
+    let timeBlock = ((+time) < hour) ? "Event Past":"Event to come";
+    document.getElementById("demo5").innerHTML = timeBlock;
+};
 // };
 
+$(document).ready(function() {
+    $("#demo5").css("background-color", "yellow");
+});
+
+$(document).ready(function() {
+    $("#eventBlock9").css("background-color", "green");
+});
+
+$(document).ready(function() {
+    $("#eventBlock10").css("background-color", "red");
+});
+
+$(document).ready(function() {
+    $("#eventBlock11").css("background-color", "gray");
+});
+
+// $(document).ready(function() {
+// if ((timeBlock) === "Event to come") {
+//     $("#demo5").css("background-color", "blue");
+// } else {
+//     $("#demo5").css("background-color", "red");
+// }});
 
 
-const hour = new Date().getHours();
-let colour; 
 
-if (hour < 18) {
-$("h2").addClass("green");
-} else {
-    $("h2").addClass("green");
-}
+// function myFunction4 (){
+//     let time = document.getElementById("time").textContent;
+//     let timeBlock = ((+time) < hour) ? "Event Past":"Event to come";
+//     document.getElementById("demo5").innerHTML = timeBlock;
+// }
 
-document.getElementById("demo").innerHTML = colour;
+
+//     let timeBlock = document.getElementById("timeBlock").textContent;
+//     document.getElementById("eventBlock").innerHTML = timeBlock;
+
+// function checkTime () {
+//     let timeBlock = document.getElementById("timeBlock").textContent;
+//     let status = ((+time) < hour) 
+//     document.getElementById("eventBlock").innerHTML = status;
+// }
+
+// / Click event causes alert light theme toggle //.on convenience
+
+
+
+// $(document).ready(checkTime(){
+
+// Click event causes alert light theme toggle //.on convenience
+// themeButtonEl.on('click', function () {
+//     if (isDark) {
+//       $('body').css({ 'background-color': '#d9e9e8', color: '#1a1a1a' });
+//       isDark = !isDark;
+//     } else {
+//       $('body').css({ 'background-color': '#1a1a1a', color: '#d9e9e8' });
+//       isDark = !isDark;
+//     }
+//   });
+
+// $(document).ready(function(){
+//     if ("col-2" !== hour) {
+//         $("input").css("background-color", "#EC7063");
+//     } else {
+//     $("input").css("background-color", "#52BE80");
+//     }
+// });
+
+//     saveBtn.on('click', function () {
+//         if (timeBlock) {
+//             $('eventBlock').css({ 'background-color': '#d9e9e8', color: '#1a1a1a' });
+//             timeBlock < hour;
+//         } else {
+//             $('eventBlock').css({ 'background-color': '#1a1a1a', color: '#d9e9e8'  });
+//         }
+//     });
+
+
+
+//     saveBtn.on('click', function () {
+//         if (timeBlock) {
+//             $('eventBlock').css({ 'background-color': '#d9e9e8', color: '#1a1a1a' });
+//             timeBlock < hour;
+//         } else {
+//             $('eventBlock').css({ 'background-color': '#1a1a1a', color: '#d9e9e8'  });
+//         }
+//     });
+
